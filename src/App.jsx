b9ce57039724687ee5872aa80747e651c480b898
Ley official <danyilfreelance@gmail.com>
2023-09-20 18:01:42 +0100

    tests1

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          coun d t is {count}
+          coun d dsdsa dt is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
