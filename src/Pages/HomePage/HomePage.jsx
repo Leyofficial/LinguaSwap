5f3843de137878da311f859765922a1cbe25fc9c
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-09-22 22:06:52 +0200

    created main info , created custom button

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,39 +1,50 @@
 
 import style from "./HomePage.module.scss";
-import { NavLink } from "react-router-dom";
 import firstImage from "../../images/HomePage/first.png"
 import secondImage from "../../images/HomePage/second.png";
 import thirdImage from "../../images/HomePage/third.png";
 import Header from "../../Components/Header/index.jsx";
+import {TypeAnimation} from "react-type-animation";
+import CustomButton from "../../Utility/CustomButton/CustomButton.jsx";
 
 const HomePage = () => {
   return (
     <>
-      <div className={style.containerW}>
-        <Header></Header>
-      </div>
 
-      {/*<div className={style.container}>*/}
-      {/*  <div className={style.wrapperText}>*/}
-      {/*    <h1>Open doors with language learning</h1>*/}
-      {/*    <p>*/}
-      {/*      Connect with native speakers around the world for language exchange,*/}
-      {/*      practice speaking and monitor your progress while having fun!*/}
-      {/*    </p>*/}
-      {/*    <NavLink to={"/"}>Start to learn</NavLink>*/}
-      {/*  </div>*/}
-      {/*  <div className={style.containerImages}>*/}
-      {/*    <div className={style.wrapperImages + " " + style.firstImage}>*/}
-      {/*      <img alt={"home-image"} src={firstImage} />*/}
-      {/*    </div>*/}
-      {/*    <div className={style.wrapperImages + " " + style.secondImage}>*/}
-      {/*      <img alt={"home-image"} src={secondImage} />*/}
-      {/*    </div>*/}
-      {/*    <div className={style.wrapperImages + " " + style.thirdImage}>*/}
-      {/*      <img alt={"home-image"} src={thirdImage} />*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
+      <div className={style.container}>
+        <div className={style.header}>
+          <Header></Header>
+        </div>
+        <div className={style.wrapperHome}>
+          <div className={style.wrapperText}>
+            <h1>Lin<span style={{fontStyle:'normal'}}>gua</span> is a service for <span>learning languages</span> using</h1>
+            <div className={style.wrapperType}>
+              <TypeAnimation sequence={[
+                'Student',4000,'Teacher',3000
+              ]} wrapper={'span'} speed={50} style={{ display: 'inline-block' }} repeat={Infinity}></TypeAnimation>
+            </div>
+            <div className={style.wrapperInfo}>
+              <p>
+                Connect with native speakers around the world for language exchange,
+                practice speaking and monitor your progress while having fun!
+              </p>
+            </div>
+            <CustomButton title={'Start to learn'} callback={null} typeOfButton={'link'} path={'login'}></CustomButton>
+
+          </div>
+          <div className={style.containerImages}>
+            <div className={style.wrapperImages + " " + style.firstImage}>
+              <img alt={"home-image"} src={firstImage} />
+            </div>
+            <div className={style.wrapperImages + " " + style.secondImage}>
+              <img alt={"home-image"} src={secondImage} />
+            </div>
+            <div className={style.wrapperImages + " " + style.thirdImage}>
+              <img alt={"home-image"} src={thirdImage} />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
