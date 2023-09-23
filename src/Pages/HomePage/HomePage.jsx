7e0a5f0c28a312910b7bc3f92f62b62e19ca448b
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-09-23 17:13:27 +0200

    did simple refactor for HomePage Component

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,13 +1,9 @@
-
 import style from "./HomePage.module.scss";
-import firstImage from "../../images/HomePage/first.png"
-import secondImage from "../../images/HomePage/second.png";
-import thirdImage from "../../images/HomePage/third.png";
 import Header from "../../Components/Header/index.jsx";
-import {TypeAnimation} from "react-type-animation";
-import CustomButton from "../../Utility/CustomButton/CustomButton.jsx";
 import {useDispatch} from "react-redux";
 import {moveToLogin} from "../../Redux/isStartToLogin/isStartToLoginAC.js";
+import HomeText from "./HomeText/HomeText.jsx";
+import HomeImages from "./HomeImages/HomeImages.jsx";
 
 const HomePage = () => {
 
@@ -25,33 +21,8 @@ const HomePage = () => {
           <Header></Header>
         </div>
         <div className={style.wrapperHome}>
-          <div className={style.wrapperText}>
-            <h1>Lin<span style={{fontStyle:'normal'}}>gua</span> is a service for <span>learning languages</span> using</h1>
-            <div className={style.wrapperType}>
-              <TypeAnimation sequence={[
-                'Student',4000,'Teacher',3000
-              ]} wrapper={'span'} speed={50} style={{ display: 'inline-block' }} repeat={Infinity}></TypeAnimation>
-            </div>
-            <div className={style.wrapperInfo}>
-              <p>
-                Connect with native speakers around the world for language exchange,
-                practice speaking and monitor your progress while having fun!
-              </p>
-            </div>
-            <CustomButton title={'Start to learn'} callback={toLogin} typeOfButton={'link'} path={'login'}></CustomButton>
-
-          </div>
-          <div className={style.containerImages}>
-            <div className={style.wrapperImages + " " + style.firstImage}>
-              <img alt={"home-image"} src={firstImage} />
-            </div>
-            <div className={style.wrapperImages + " " + style.secondImage}>
-              <img alt={"home-image"} src={secondImage} />
-            </div>
-            <div className={style.wrapperImages + " " + style.thirdImage}>
-              <img alt={"home-image"} src={thirdImage} />
-            </div>
-          </div>
+          <HomeText toLogin={toLogin}></HomeText>
+          <HomeImages></HomeImages>
         </div>
       </div>
     </>
