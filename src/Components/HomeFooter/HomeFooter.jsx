d6edfed6ba4083f67e05b52c9aaaae5a6ad09bb8
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-09-25 16:12:35 +0200

    added a link for footer logo

--- src/Components/HomeFooter/HomeFooter.jsx
+++ src/Components/HomeFooter/HomeFooter.jsx
@@ -2,21 +2,31 @@ import style from './HomeFooter.module.scss'
 import {LiaTelegramPlane} from "react-icons/lia";
 import {BiLogoInstagram, BiLogoTwitter} from "react-icons/bi";
 import logoImage from '../../img/icons/ukraine.png'
+import NetworkItem from "./NetworkItem/NetworkItem.jsx";
 
 const HomeFooter = () => {
+
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      // left: 0,
+      behavior: "smooth",
+    })
+  }
+
   return (
     <div className={style.container}>
       <div className={style.wrapper}>
-        <div>
+        <div onClick={scrollUp}>
           <h2 className={style.logo}>Lin<span>gua</span></h2>
           <img src={logoImage} alt={'logo'}/>
         </div>
 
         <p>Copyright© 2023 LinguaSwap</p>
         <div className={style.wrapperIcons}>
-          <LiaTelegramPlane></LiaTelegramPlane>
-          <BiLogoInstagram></BiLogoInstagram>
-          <BiLogoTwitter></BiLogoTwitter>
+          <NetworkItem path={'https://t.me/temcenko'} icon={<LiaTelegramPlane></LiaTelegramPlane>}></NetworkItem>
+          <NetworkItem path={'https://www.instagram.com/'} icon={<BiLogoInstagram></BiLogoInstagram>}></NetworkItem>
+          <NetworkItem path={'https://twitter.com/'} icon={<BiLogoTwitter></BiLogoTwitter>}></NetworkItem>
         </div>
       </div>
     </div>
