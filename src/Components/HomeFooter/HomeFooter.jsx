5d9c725214f18877e245f27972294e2545a4b4cb
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-09-25 15:57:14 +0200

    created style for footer

--- src/Components/HomeFooter/HomeFooter.jsx
+++ src/Components/HomeFooter/HomeFooter.jsx
@@ -1,13 +1,15 @@
 import style from './HomeFooter.module.scss'
 import {LiaTelegramPlane} from "react-icons/lia";
 import {BiLogoInstagram, BiLogoTwitter} from "react-icons/bi";
+import logoImage from '../../img/icons/ukraine.png'
 
 const HomeFooter = () => {
   return (
     <div className={style.container}>
       <div className={style.wrapper}>
         <div>
-          <h2>Lingua</h2>
+          <h2 className={style.logo}>Lin<span>gua</span></h2>
+          <img src={logoImage} alt={'logo'}/>
         </div>
 
         <p>Copyright© 2023 LinguaSwap</p>
