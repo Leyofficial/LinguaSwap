147878bacd08438d08ed2825d0809f0620467822
Ley official <danyilfreelance@gmail.com>
2023-09-26 22:40:08 +0100

    create-aboutYor-section

--- src/Utility/CustomInput/CustomInput.jsx
+++ src/Utility/CustomInput/CustomInput.jsx
@@ -2,15 +2,23 @@ import { useState } from "react";
 import style from "./CustomInput.module.scss";
 
 const CustomInput = (props) => {
-  const [input , setInput] = useState('')  
-  const { width, callback , placeholder , heg} = props;
+  const [input, setInput] = useState("");
+  const { width = 500, callback, placeholder, heg } = props;
   return (
     <div className={style.container}>
-      <div className={style["wave-group"]}>
-        <input onFocus={() =>  heg ? setInput('#') : setInput(null) } value={input} style={{width : width  }} placeholder={placeholder} required="" type="text" className={style.input} />
-        <span className={style.bar}></span>
-        <label className={style.label}>
-        </label>
+      <div className={style["wave-group"]} style={{ width: width }}>
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          onFocus={() => (heg ? setInput( "#" + input ) : setInput(null))}
+          value={input}
+          style={{ width: width }}
+          placeholder={placeholder}
+          required=""
+          type="text"
+          className={style.input}
+        />
+        <span className={style.bar} style={{ width: width }}></span>
+        <label className={style.label} style={{ width: width }}></label>
       </div>
     </div>
   );
