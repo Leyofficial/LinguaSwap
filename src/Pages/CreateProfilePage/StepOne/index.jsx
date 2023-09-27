a1090ea14330a7b0a3d1222b163d5148e7e386e2
Ley official <danyilfreelance@gmail.com>
2023-09-27 20:18:27 +0100

    fixed-file

--- src/Pages/CreateProfilePage/StepOne/index.jsx
+++ src/Pages/CreateProfilePage/StepOne/index.jsx
@@ -59,7 +59,7 @@ const StepOne = (props) => {
     return (
         <>
             <h2 className={style.title}>
-                <b>About you :</b>
+                <b>About you test:</b>
             </h2>
             <div className={style.container}>
                 <div className={style.wrapper}>
