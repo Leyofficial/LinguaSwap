e1bc3089c4f1d7e0f419adfa263bc878d92102cc
Ley official <danyilfreelance@gmail.com>
2023-09-29 21:47:51 +0100

    step-two-ui

--- src/Pages/CreateProfilePage/StepTwo/index.jsx
+++ src/Pages/CreateProfilePage/StepTwo/index.jsx
@@ -1,28 +1,33 @@
-
 import CustomButton from "../../../Utility/CustomButton/CustomButton";
 import style from "./StepTwo.module.scss";
+import {Avatar} from "@mui/material";
+import {ImageInput} from "../../../Utility/InputImage/InputImage.jsx";
+
 const StepTwo = (props) => {
-  return (
-    <>
-      <h2 className={style.title}>
-        <b>Education</b> (option)
-      </h2>
-      <div className={style.container}>
-        <div className={style.buttonsTwo}>
-          <CustomButton
-            title={"Before"}
-            typeOfButton={"button"}
-            callback={props.previousStep}
-          />
-          <CustomButton
-            title={"Next"}
-            typeOfButton={"button"}
-            callback={props.nextStep}
-          />
+    return (
+        <div className={style.wholeContent}>
+            <h2 className={style.title}>
+                <b>Make your profile beautiful !</b>
+            </h2>
+            <div className={style.container}>
+                <div className={style.containerBlock}>
+                    <ImageInput/>
+                </div>
+                <div className={style.buttonsTwo}>
+                    <CustomButton
+                        title={"Before"}
+                        typeOfButton={"button"}
+                        callback={props.previousStep}
+                    />
+                    <CustomButton
+                        title={"Next"}
+                        typeOfButton={"button"}
+                        callback={props.nextStep}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </>
-  );
+);
 };
 
 export default StepTwo;
