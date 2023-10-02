07b083a2f8be91450e8a3f2c98cba2a097f32252
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-10-02 17:32:27 +0200

    added applyMiddleware

--- src/Redux/rootReduce.js
+++ src/Redux/rootReduce.js
@@ -1,4 +1,4 @@
-import {  createStore } from "redux";
+import {applyMiddleware, createStore} from "redux";
 import { initialState } from "./initialState";
 
 import combinedReducers from './combineReducer'
