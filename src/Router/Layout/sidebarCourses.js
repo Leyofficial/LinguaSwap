488c52c82658414bfcf4fc5e0113dc61b1440dc9
Ley official <danyilfreelance@gmail.com>
2023-10-04 22:33:30 +0100

    added-sidebarCourses

---
+++ src/Router/Layout/sidebarCourses.js
@@ -0,0 +1,17 @@
+import info from "../../img/icons/info.png";
+import previous from  "../../img/icons/previous.png"
+
+export const sidebarCourses = [
+    {
+
+        path: "/",
+        icon: previous,
+        name: "Back",
+    },
+    {
+
+        path: "/",
+        icon: info,
+        name: "Courses",
+    },
+];
