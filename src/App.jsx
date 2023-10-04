1d57d029a5460fedf2fe7920f34335c92e8ef603
Ley official <danyilfreelance@gmail.com>
2023-10-04 22:31:08 +0100

    set-up-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {Route} from "react-router-dom";
 import Layout from "./Router/Layout/Layout";
-import { Routes } from "react-router-dom";
+import {Routes} from "react-router-dom";
 import Login from "./Components/Login/Login";
 import TeacherRegister from "./Components/TeacherRegister/TeacherRegister";
 import './App.css'
@@ -14,32 +14,34 @@ import CourseSection from "./Pages/CourseSection/CourseSection.jsx";
 
 
 function App() {
+    const isStart = useSelector((state) => state.isStart)
+    return (
+        <>
+            <Routes>
+                <Route path={'/'} element={<Layout/>}>
+                    <Route index={true} element={isStart ? <CoursesSection/> : <HomePage/>}/>
+                    <Route path={'aboutApp/:userType'} element={<AboutAppPage/>}></Route>
+                        <Route path={"/login"} element={<Login/>}/>
+                        <Route path={"/teacherregister"} element={<TeacherRegister/>}/>
+                        <Route path={"/createprofile"} element={<CreateProfile/>} />
+                        <Route path={"/course/:idCourse"} element={<CourseSection/>}></Route>
+                    <Route path={"*"} element={<ErrorUrl/>}/>
+                </Route>
+            </Routes>
 
-  const isStart = useSelector((state) => state.isStart);
-  return (
-    <>
-       {!isStart ?
-         <Routes>
-           <Route path={'/'} element={<Layout layoutType={'home'}/>}>
-             <Route index={true} element={<HomePage/>}></Route>
-             <Route path={'aboutApp/:userType'} element={<AboutAppPage/>}></Route>
-             <Route path={"*"} element={<ErrorUrl/>} />
-           </Route>
-         </Routes>
 
-        :
-        <Routes>
-          <Route path={"/"} element={<Layout layoutType={'main'}/>}>
-            <Route index={true} element={ <CoursesSection/>}></Route>
-            <Route path={"/login"} element={<Login/>}/>
-            <Route path={"/teacherregister"} element={<TeacherRegister/>}/>
-            <Route path={"/createprofile"} element={<CreateProfile/>} />
-            <Route path={"/course/:idCourse"} element={<CourseSection/>}></Route>
-            <Route path={"*"} element={<ErrorUrl/>} />
-          </Route>
-        </Routes>}
-    </>
-  );
+            {/*<Routes>*/}
+            {/*  <Route path={"/"} element={<Layout layoutType={'main'}/>}>*/}
+            {/*    <Route index={true} element={ <CoursesSection/>}></Route>*/}
+            {/*    <Route path={"/login"} element={<Login/>}/>*/}
+            {/*    <Route path={"/teacherregister"} element={<TeacherRegister/>}/>*/}
+            {/*    <Route path={"/createprofile"} element={<CreateProfile/>} />*/}
+            {/*    <Route path={"/course/:idCourse"} element={<CourseSection/>}></Route>*/}
+            {/*    <Route path={"*"} element={<ErrorUrl/>} />*/}
+            {/*  </Route>*/}
+            {/*</Routes>}*/}
+        </>
+    );
 }
 
 export default App;
