5b6976943f3e04fd737068224c7180f0c991b1c9
Ley official <danyilfreelance@gmail.com>
2023-10-06 21:15:25 +0100

    add-get-teachers-from-server

--- src/Pages/TeachersSection/index.jsx
+++ src/Pages/TeachersSection/index.jsx
@@ -5,33 +5,30 @@ import {Select, Space} from "antd";
 import TeacherCard from "./TeacherCard/TeacherCard.jsx";
 import photo1 from './../../../uploads/30092023-092955_618-PaleMask_burning_heart_4c63719d-153d-4bbb-847a-ed651e2b1d81.png'
 import Spinner from "../../Utility/Spinner/Spinner.jsx";
+import axios from "axios";
+import {Teachers} from "../../ApiRequests/Teacher/Teachers.js";
+import {useDispatch, useSelector} from "react-redux";
+import {teachersActionCreater} from "../../Redux/Teachers/teachersActionCreater.js";
 
 const TeachersSection = () => {
     const [searchValue, setSearchValue] = useState("")
+    const [enrolment, setEnrolment] = useState("")
+    const [languageFilter, setLanguageFilter] = useState('')
+    const [category, setCategory] = useState('')
+    const [foundTeacher, setFoundTeacher] = useState(null)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+       Teachers.getTeachers().then(res => dispatch(teachersActionCreater(res.data.users)))
+    }, [])
+
+    const teachers = useSelector((state) => state.teachers);
 
     const languages = ['All', 'English', 'Poland', 'Germany', 'Spanish', 'Italy', 'Japan', 'Turkish']
     const enrolmentType = ["All", 'Free', 'Paid']
-    const [enrolment, setEnrolment] = useState("")
-    const [languageFilter, setLanguageFilter] = useState('')
     const categoryTypes = ["All", 'Popular', 'Recent']
-    const [category, setCategory] = useState('')
-    const [foundTeacher, setFoundTeacher] = useState(null)
 
-    const searchTeacherItem = () => teachers.filter(item => item.hash.toLowerCase().includes(searchValue.toLowerCase()))
-
-    const teachers = [
-        {name: 'Danyil', photo: photo1, hash: 'ley', languages: []},
-        {name: 'Danyil', photo: photo1, hash: 'ley', languages: []},
-        {name: 'Danyil', photo: photo1, hash: 'ley', languages: []},{name: 'Danyil', photo: photo1, hash: 'ley', languages: []},
-        {name: 'Danyil', photo: photo1, hash: 'ley', languages: []},{name: 'Danyil', photo: photo1, hash: 'ley', languages: []},
-        {name: 'Danyil', photo: photo1, hash: 'ley', languages: []},
-        {name: 'Danyil', photo: photo1, hash: 'ley', languages: []},
-        {name: 'Danyil', photo: photo1, hash: 'ley', languages: []},
-        {name: 'Danyil', photo: photo1, hash: 'ley', languages: []},
-        {name: 'Danyil', photo: photo1, hash: 'ley', languages: []},
-        {name: 'Danyil', photo: photo1, hash: 'ley', languages: []},
-        {name: 'Andrew', photo: 'Ya', hash: 'aay', languages:  [{ label : 'English' , color :  '#ff0000' } , {label : "Ukrainian" , color : "#00a6ff"} , { label : 'English' , color :  '#5e134e' }]}
-    ];
+    const searchTeacherItem = () => teachers.filter(item => item.user.data.userTag.toLowerCase().includes(searchValue.toLowerCase()))
 
     useEffect(() => {
         if (searchValue) {
@@ -82,9 +79,9 @@ const TeachersSection = () => {
                 <div className={style.teachersBlock}>
                     <ul className={style.cardsItems}>
                         { !foundTeacher ?  teachers.map((item) => {
-                            return <TeacherCard name={item.name} hash={item.hash} photo={item.photo} languages={item.languages}/>
+                            return <TeacherCard name={item.user.data.name} hash={item.user.data.userTag} photo={item.user.data.photo} languages={item.user.data.languagesKnow}/>
                         }) : foundTeacher.map((item) => {
-                            return <TeacherCard name={item.name} hash={item.hash} photo={item.photo} languages={item.languages}/>
+                            return <TeacherCard name={item.user.data.name} hash={item.user.data.userTag} photo={item.user.data.photo} languages={item.user.data.languagesKnow}/>
                         })}
                     </ul>
                 </div>
