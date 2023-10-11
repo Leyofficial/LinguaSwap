467a9ec876ab8b4c7802d569f30857cebe2e2c52
temcenkovolo <temchenkovolo@gmail.com>
2023-10-11 07:05:12 +0200

    fixed

--- src/Redux/Course/Chat/CourseChatReducer.js
+++ src/Redux/Course/Chat/CourseChatReducer.js
@@ -1,13 +1,14 @@
 import {initialState} from "../../initialState.js";
 
-
+export const RESET_CHAT = "RESET_CHAT"
 export const SET_CHAT = "SET_CHAT"
 
 const CourseChatReducer = (chat = initialState.currentChat,action) =>{
    switch (action.type) {
 
       case SET_CHAT : return action.chatData
-
+      case RESET_CHAT :
+         return null
       default : return chat
    }
 }
