d6a5976c02fc40f73aeb9ff98877df3b02b3ba45
temcenkovolo <temchenkovolo@gmail.com>
2023-10-11 07:42:14 +0200

    fix files

--- src/Pages/CourseChat/CourseChat.jsx
+++ src/Pages/CourseChat/CourseChat.jsx
@@ -1,37 +1,31 @@
 import React, {useEffect, useState} from 'react';
 import style from './CourseChat.module.scss'
 import {useDispatch, useSelector} from "react-redux";
-import {LuSend} from "react-icons/lu";
 import {getUser} from "../../ApiRequests/Courses/AuthUser.js";
-import {AiOutlinePaperClip} from "react-icons/ai";
 import {getChat, sendMessage} from "../../ApiRequests/Chat.jsx";
 import {courseChatAC, resetChatItems} from "../../Redux/Course/Chat/CourseChatAC.js";
-import Message from "./Members/MemberChat/Dialog/Message/Message.jsx";
 import {HiUserGroup} from "react-icons/hi";
 import {FaChalkboardTeacher} from "react-icons/fa";
 import {BsInfoCircle} from "react-icons/bs";
 import CourseTeachers from "./CourseTeacher/CourseTeachers.jsx";
 import CourseMember from "./CourseMembers/CourseMember.jsx";
-import {Course} from "../../ApiRequests/Courses/Courses.js";
 import {useParams} from "react-router";
+import {Course} from "../../ApiRequests/Courses/Courses.js";
+import {GiImbricatedArrows} from "react-icons/gi";
+import MessagesSection from "./Members/MemberChat/Dialog/MessagesSection/MessagesSection.jsx";
 
 
 const CourseChat = () => {
 
-   // const currentCourse = useSelector((state) => state.currentCourse)
    const [currentCourseTeacher, setCurrentCourseTeacher] = useState(null)
    const dispatch = useDispatch()
    const currentUser = useSelector((state) => state.loginUser)
-   const [message, setMessage] = useState("")
    const chat = useSelector((state) => state.currentChat)
-
-   const asideItems = ["teachers","students","info"]
-   const [asideItem,setAsideItem] = useState("teachers")
+   const [asideItem, setAsideItem] = useState("teachers")
+   const [currentCourse, setCurrentCourse] = useState(null)
    const {idCourse} = useParams()
+   const [hideInfoBlock,setHideInfoBlock] = useState(false)
 
-   const [currentCourse,setCurrentCourse] = useState(null)
-
-   console.log(currentCourse)
 
    useEffect(() => {
 
@@ -83,57 +77,51 @@ const CourseChat = () => {
          console.log("Write some text pls ")
       }
    }
+
    return (
+
       <div className={style.container}>
-         <div className={style.wrapperChat}>
-            <h2><span>{currentCourse?.course.name}</span> course chat </h2>
-            <div className={style.contentMessage}>
-               <div className={style.wrapperMessages}>
-                  <Message messages={chat?.messages}></Message>
-               </div>
+         <MessagesSection title={"course chat"} name={currentCourse?.course.name} messages={chat?.messages}
+                          sendMessageHandler={sendMessageHandler}
+         ></MessagesSection>
+
+         <div className={` ${hideInfoBlock ? style.hide : style.wrapperMebmers}`}>
+            <div className={`${style.hideBlock} ${hideInfoBlock ? style.reverseIcons : null}`} onClick={() => setHideInfoBlock(!hideInfoBlock)}>
+               <GiImbricatedArrows></GiImbricatedArrows>
             </div>
-            <div className={style.wrapperTextarea}>
-               <AiOutlinePaperClip fontSize={40}></AiOutlinePaperClip>
-               <div className={style.textarea}>
-                  <textarea placeholder={'Type a message'} value={message}
-                            onChange={(e) => setMessage(e.target.value)}></textarea>
-               </div>
-
-               <div className={style.icons}>
-                  <LuSend onClick={sendMessageHandler} fontSize={40} color={'rgba(12,87,197,0.98)'}></LuSend>
-               </div>
-
-            </div>
-         </div>
-
-         <div className={style.wrapperMebmers}>
-
             <div className={style.members}>
                <div className={style.wrapperItems}>
-               <div className={`${style.wrapperIconsMember} ${asideItem === "teachers" ? style.activeItem : null}`} onClick={() => setAsideItem("teachers")}>
-                  <FaChalkboardTeacher></FaChalkboardTeacher>
-               </div>
-               <div className={`${style.wrapperIconsMember} ${asideItem === "students" ? style.activeItem : null}`} onClick={() => setAsideItem("students")}>
-                  <HiUserGroup></HiUserGroup>
-               </div>
-               <div className={`${style.wrapperIconsMember} ${asideItem === "info" ? style.activeItem : null}`} onClick={() => setAsideItem("info")}>
-                  <BsInfoCircle></BsInfoCircle>
-               </div>
-               </div>
-               {asideItem === 'teachers' ? <CourseTeachers currentCourseTeacher={currentCourseTeacher}></CourseTeachers> :
-               asideItem === 'students' ? <div className={style.memberItems}>
-                  <h3>Students</h3>
-                  <div className={style.items}>
-                     {currentCourse.course.members.map((member,index) => <CourseMember index={index} member={member}></CourseMember>)}
+                  <div className={`${style.wrapperIconsMember} ${asideItem === "teachers" ? style.activeItem : null}`}
+                       onClick={() => setAsideItem("teachers")}>
+                     <FaChalkboardTeacher></FaChalkboardTeacher>
                   </div>
-               </div> : <div className={style.memberItems}>
-                  <h3>INFO</h3>
-                  <div className={style.items}>
-                    <div>Will be develop in the future</div>
+                  <div className={`${style.wrapperIconsMember} ${asideItem === "students" ? style.activeItem : null}`}
+                       onClick={() => setAsideItem("students")}>
+                     <HiUserGroup></HiUserGroup>
+                  </div>
+                  <div className={`${style.wrapperIconsMember} ${asideItem === "info" ? style.activeItem : null}`}
+                       onClick={() => setAsideItem("info")}>
+                     <BsInfoCircle></BsInfoCircle>
                   </div>
                </div>
-               }
+               <article className={style.wrapper}>
+                  {asideItem === 'teachers' ?
+                     <CourseTeachers currentCourseTeacher={currentCourseTeacher}></CourseTeachers> :
+                     asideItem === 'students' ? <div className={style.memberItems}>
+                        <h3>Students</h3>
+                        <div className={style.items}>
+                           {currentCourse?.course.members.map((member, index) => <CourseMember index={index}
+                                                                                               member={member}></CourseMember>)}
+                        </div>
+                     </div> : <div className={style.memberItems}>
+                        <h3>INFO</h3>
+                        <div className={style.items}>
+                           <div>Will be develop in the future</div>
+                        </div>
+                     </div>
 
+                  }
+               </article>
             </div>
          </div>
       </div>
