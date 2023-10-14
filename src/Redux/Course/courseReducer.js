26cf6a74af36837c4b6c6f1f14c8704e6fef53bb
temcenkovolo <temchenkovolo@gmail.com>
2023-10-14 10:56:21 +0200

    created thunk creator for course requests

--- src/Redux/Course/courseReducer.js
+++ src/Redux/Course/courseReducer.js
@@ -1,12 +1,50 @@
 import {initialState} from "../initialState.js";
+import {Course} from "../../ApiRequests/Courses/Courses.js";
+import {courseAC} from "./CourseAC.js";
+import {teacherChats} from "../../ApiRequests/TeacherChats/TeacherChats.js";
 
 export const SET_COURSE = "SET_COURSE"
 
- const courseReducer = (course = initialState.currentCourse,action) => {
+const courseReducer = (course = initialState.currentCourse, action) => {
    switch (action.type) {
-      case SET_COURSE: return action.newCourse
-      default:return course
+      case SET_COURSE:
+         return action.newCourse
+      default:
+         return course
    }
 }
 
 export default courseReducer
+
+export const getCourseThunkCreator = (idCourse) => {
+   return async (dispatch) => {
+      try {
+         const response = await Course.getCourse(idCourse)
+         if (response.status === 200) {
+            dispatch(courseAC(response.data.course))
+         }
+      } catch (err) {
+
+         console.log(err)
+      }
+
+   }
+}
+
+export const joinToCourseAndCreateChatThunkCreator = (courseId, teacherId, userId) => {
+   return async (dispatch) => {
+      try {
+         await teacherChats.createChat(teacherId, userId)
+         const joinCourseResponse = await Course.addNewMember(userId, courseId)
+
+         if (joinCourseResponse.status === 200) {
+            const response = await Course.getCourse(courseId)
+            if (response.status === 200) {
+               dispatch(courseAC(response.data.course))
+            }
+         }
+      } catch (err) {
+         console.log(err)
+      }
+   }
+}
