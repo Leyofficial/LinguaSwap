a287b02c4af35e51f0653dc18895bc6d5f31d31a
temcenkovolo <temchenkovolo@gmail.com>
2023-10-14 10:00:42 +0200

    added semantic for CourseBlock

--- src/Pages/CoursesSection/CoursesBlock/CoursesBlock.jsx
+++ src/Pages/CoursesSection/CoursesBlock/CoursesBlock.jsx
@@ -17,20 +17,24 @@ const CoursesBlock = ({course}) => {
   return (
     <article className={style.container}>
       <NavLink to={`/course/${course._id}`}>
-      <div className={style.containerWrapper}>
-        <div className={style.courseHeader}>
+
+      <section className={style.containerWrapper}>
+
+        <header className={style.courseHeader}>
           <img  src={course.course.image ? course.course.image : defaultImage} alt={'course'}/>
           <div className={style.language}>
             <p>{course.course.language}</p>
           </div>
-        </div>
-        <div className={style.authorWrapper}>
+        </header>
+
+        <figure className={style.authorWrapper}>
           <img src={test} alt={'author'}/>
-        </div>
-        <div className={style.nameOfAuthor}>
-          <p>Tamara Petrovna</p>
-        </div>
-        <div className={style.wrapper}>
+          <figcaption className={style.nameOfAuthor}>
+            <p>Tamara Petrovna</p>
+          </figcaption>
+        </figure>
+
+        <section className={style.wrapper}>
           <div className={style.titleWrapper}>
             <h3>{course.course.name}</h3>
           </div>
@@ -41,19 +45,22 @@ const CoursesBlock = ({course}) => {
             <p style={levelEducation(course.course.level)}>{course.course.level}</p>
            <p className={style.duration}>{course.course.duration}</p>
           </div>
-          <div className={style.wrapperLevel}>
+
+          <section className={style.wrapperLevel}>
             <div>
               <p className={style.startDate}><span className={style.icon}><GiClockwork/>Start</span><span>{course.course.startCourse}</span></p>
               <p className={style.finishDate}><span className={style.icon}><GiClockwork/>End</span><span>{course.course.finishCourse}</span></p>
             </div>
-          </div>
-          <div className={style.author}>
+          </section>
+
+          <section className={style.author}>
             <div>{dateOfCourse(course.course.startCourse) ?
               <p className={style.start}>Enrol</p> :
               <p className={style.finish}>Now</p>}</div>
-          </div>
-        </div>
-      </div>
+          </section>
+
+        </section>
+      </section>
       </NavLink>
     </article>
   );
