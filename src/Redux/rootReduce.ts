81e33ac1524958c82fde48daed240c42b036d6ff
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-10-20 17:40:28 +0200

    changed mainChatsReducer

--- src/Redux/rootReduce.ts
+++ src/Redux/rootReduce.ts
@@ -2,7 +2,8 @@ import {applyMiddleware, createStore} from "redux";
 
 
 import thunk from "redux-thunk";
-import rootReducer from "./combineReducer.ts";
+import {rootReducer} from "./combineReducer.ts";
+
 
 const store = createStore(
     rootReducer,
