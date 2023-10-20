913e58c5c0f1051c21eca92ec20f37876e767225
Ley official <danyilfreelance@gmail.com>
2023-10-20 16:00:30 +0100

    add-merge-conflict

--- src/Redux/rootReduce.ts
+++ src/Redux/rootReduce.ts
@@ -1,6 +1,7 @@
 import {applyMiddleware, createStore} from "redux";
 
 import thunk from "redux-thunk";
+import {rootReducer} from "./combineReducer.ts";
 
 const store = createStore(
     rootReducer,
