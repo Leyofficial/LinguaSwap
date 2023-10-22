e8ee951f9a7c657fe81480ae6075c2931666c162
Ley official <danyilfreelance@gmail.com>
2023-10-22 13:21:17 +0100

    add-types-courses

---
+++ src/types/coursesTypesLanguages.ts
@@ -0,0 +1,19 @@
+export enum ILanguageItems {
+    All = 'All',
+    English ='English',
+    Poland = 'Poland',
+    Germany = 'Germany',
+    Spanish = 'Spanish',
+    Italy =  'Italy',
+    Japan = 'Japan',
+    Turkish = 'Turkish'
+}
+
+export enum IEnrolmentType {
+    All = 'All',
+    Free = 'Free',
+    Paid = 'Paid'
+}
+
+export type TLanguagesItems = ILanguageItems
+export type TEnrolmentType = IEnrolmentType
