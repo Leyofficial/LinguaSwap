e16479474da2a4cef6c3aeb6e867d23e5a17c380
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-10-23 21:06:12 +0200

    added function to set isLoad

--- src/Pages/CoursesSection/CoursesBlock/HeaderBlock/HeaderBlock.jsx
+++ src/Pages/CoursesSection/CoursesBlock/HeaderBlock/HeaderBlock.jsx
@@ -8,18 +8,19 @@ import {Skeleton} from "@mui/material";
 const HeaderBlock = ({course}) => {
    const [avatar, setAvatar] = useState("")
    const [avatarCourse,setAvatarCourse] = useState("")
-   const [isLoad,setIsLoad] = useState(false)
+   const [isLoadAvatarCourse,setIsLoadAvatarCourse] = useState(false)
+   const [isLoadAvatarUser,setIsLoadAvatarUser] = useState(false)
    useEffect(() => {
       if (course) {
          const getAuthorAvatar = async () => {
             const gotAvatar = await getUser(course.teacher.id)
 
             if (gotAvatar.status === 200) {
-               getImageFromServer(gotAvatar.data.user.user.data.photo, setAvatar,setIsLoad)
+               getImageFromServer(gotAvatar.data.user.user.data.photo, setAvatar,setIsLoadAvatarUser)
             }
 
          }
-         getImageFromServer(course.course.image,setAvatarCourse)
+         getImageFromServer(course.course.image,setAvatarCourse,setIsLoadAvatarCourse)
          getAuthorAvatar()
       }
 
@@ -28,14 +29,14 @@ const HeaderBlock = ({course}) => {
    return (
       <>
          <header className={style.courseHeader}>
-            {isLoad ? <img src={avatarCourse ? avatarCourse : <Skeleton/>} alt={'course'}/> : <Skeleton  variant="rectangular" width={"100%"} height={200} ></Skeleton>}
+            {isLoadAvatarCourse ? <img src={avatarCourse ? avatarCourse : <Skeleton/>} alt={'course'}/> : <Skeleton  variant="rectangular" width={"100%"} height={200} ></Skeleton>}
             <div className={style.language}>
                <p>{course.course.language}</p>
             </div>
          </header>
 
          <figure className={style.authorWrapper}>
-            {isLoad ? <img src={avatar  ? avatar : ""} alt={'author'}/> : <Skeleton  variant="rectangular" width={50} height={50}/>}
+            {isLoadAvatarUser ? <img src={avatar  ? avatar : ""} alt={'author'}/> : <Skeleton  variant="rectangular" width={50} height={50}/>}
             <figcaption className={style.nameOfAuthor}>
              <p>{course.teacher.name}</p>
             </figcaption>
