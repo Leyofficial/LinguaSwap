cac470861914aa0b052737c7215dae4a7a748a39
temcenkovolo <temchenkovolo@gmail.com>
2023-10-24 10:00:44 +0200

    created App foulder

---
+++ src/App/App.jsx
@@ -0,0 +1,70 @@
+import './App.css'
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {getUserByToken} from "../ApiRequests/Courses/AuthUser.js";
+import {authAC} from "../Redux/isAuth/isAuthAC.ts";
+import {loginUserAC} from "../Redux/login/loginUserAC.ts";
+import socketIO from 'socket.io-client'
+import {webSocketAC} from "../Redux/WebSocket/webSocketReducer.js";
+import {loginUserThunkCreator} from "../Redux/login/loginUserReducer.ts";
+import {backFromLogin, moveToLogin} from "../Redux/isStartToLogin/isStartToLoginAC.ts";
+import AppRouters from "./AppRouters.jsx";
+
+function App() {
+
+   const isAuth = useSelector((state) => state.isAuth)
+   const dispatch = useDispatch()
+   const currentUser = useSelector((state) => state.loginUser)
+   const newSocket = useSelector((state) => state.socket)
+   const userToken = JSON.parse(localStorage.getItem('loginUser'));
+    const alreadyStart = JSON.parse(localStorage.getItem('alreadyStart'));
+
+    useEffect(() => {
+        if (alreadyStart) {
+            dispatch(moveToLogin());
+        } else {
+            dispatch(backFromLogin());
+        }
+    }, []);
+
+   useEffect(() => {
+      if (currentUser) {
+         const socket = socketIO.connect('https://linguaswap-9bebd1d452cf.herokuapp.com', {
+            "forceNew": true
+         })
+         dispatch(webSocketAC(socket))
+      }
+   }, [currentUser])
+
+   useEffect(() => {
+      if (userToken) {
+         loginUserThunkCreator(userToken)(dispatch)
+         dispatch(authAC())
+      }
+   }, [])
+
+   useEffect(() => {
+      if (currentUser && newSocket) {
+         newSocket.emit("newUser", currentUser?._id)
+
+    }
+  },[currentUser,newSocket])
+  useEffect(() => {
+    if (userToken && !isAuth) {
+      getUserByToken(userToken).then(res => {
+        if (res.status === 200) {
+          dispatch(loginUserAC(...res.data.users)); // (...res.data.users[0]));
+          dispatch(authAC())
+        }
+      })
+    }
+  }, [userToken, isAuth])
+
+   return (
+      <>
+      <AppRouters isAuth={isAuth}></AppRouters>
+      </>
+   );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App/App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import store from './Redux/rootReduce.ts'
 import { Provider } from "react-redux";
