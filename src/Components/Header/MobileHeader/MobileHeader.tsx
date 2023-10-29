f36bc3f7ae99c0211e26eb3c0daefb19f2c43ada
Ley official <danyilfreelance@gmail.com>
2023-10-29 10:23:15 +0000

    change-style-mobile-profile

--- src/Components/Header/MobileHeader/MobileHeader.tsx
+++ src/Components/Header/MobileHeader/MobileHeader.tsx
@@ -5,13 +5,21 @@ import {INavItems, INavWrapper} from "../../../types/headerTypes.ts";
 import {HeaderItem} from "../HeaderWrapper/HeaderItem.tsx";
 import headerIcon from "../../../img/icons/ukraine.png";
 const MobileHeader = ({navItems} : INavItems) => {
-    const [active,setActive] = useState(false)
+    const [active,setActive] = useState<boolean>(false)
     return (
         <header className={style.container}>
             <div className={active ? style.active : ""}>
-            <div className={`${style.headerBurger}`} onClick={() => setActive(!active)}>
-                <span></span>
-            </div>
+            {/*<div className={`${style.headerBurger}`} onClick={() => setActive(!active)}>*/}
+            {/*    <span></span>*/}
+            {/*</div>*/}
+                <div className={`${style.headerBurger}`}>
+                    <label className={style.burger}  htmlFor="burger">
+                        <input onChange={() => setActive(!active)} checked={active}  type="checkbox" id="burger" />
+                        <span style={ active ?  {backgroundColor : "white"} : {backgroundColor : 'dodgerblue'}}></span>
+                        <span style={ active ?  {backgroundColor : "white"} : {backgroundColor : 'dodgerblue'}}></span>
+                        <span style={ active ?  {backgroundColor : "white"} : {backgroundColor : 'dodgerblue'}}></span>
+                    </label>
+                </div>
                 <div className={style.logo}>
                     <h2 className={style.text}>
                         Lin<span style={{ color: "dodgerblue" , cursor : 'pointer'}}>gua</span>
@@ -19,7 +27,7 @@ const MobileHeader = ({navItems} : INavItems) => {
                     <img src={headerIcon} alt="icon" />
                 </div>
 
-                <div className={style.menu}>
+                <div className={style.menu} style={ active ? {top : 0} : {top : -1000}}>
                     <ul className={style.items}>
                         <div className={style.btnWrapper}>
                             <div className={style.navList}>
