bbdf43280c991cb58347b62d0dcbf47a60243bf8
Ley official <danyilfreelance@gmail.com>
2023-10-30 22:23:54 +0000

    FINAL FIXED BAG WITH PHOTO 🎉 🎉 🎉 🎉 🎉

--- src/ApiRequests/ServerFiles/getImage.ts
+++ src/ApiRequests/ServerFiles/getImage.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const getImageFromServer = async (imageName: string, callback: (arg: string) => void, isLoad?: (arg: boolean) => void) => {
+export const getImageFromServer = async (imageName: string | any , callback: (arg: string) => void, isLoad?: (arg: boolean) => void) => {
     try {
-        const response: any = await axios.get(`https://linguaswap-9bebd1d452cf.herokuapp.com/courses/upload/${imageName}`, {responseType: 'arraybuffer'})
+        const response: any = await axios.get(`https://linguaswap-9bebd1d452cf.herokuapp.com/courses/upload/${imageName.path ? imageName.path : imageName}`, {responseType: 'arraybuffer'})
         const base64 = btoa(
             new Uint8Array(response.data).reduce(
                 (data, byte) => data + String.fromCharCode(byte),
